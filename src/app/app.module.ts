89a64301451224a7065f9561b20ea9111bb32427
Famodun Gbolaga <biyibg@yahoo.co.uk>
2017-09-29 15:15:28 +0300

    first update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
+import { GithubFollowersService } from './services/github-followers.service';
+import { AppErrorHandler } from './common/app-error-handler';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GithubFollowersComponent } from './github-followers/github-followers.component';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubFollowersComponent,
+    NotFoundComponent,
+    GithubProfileComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '', 
+        component: HomeComponent
+      },
+      {
+        path: 'followers/:id/:username', 
+        component: GithubProfileComponent
+      },
+      {
+        path: 'followers', 
+        component: GithubFollowersComponent
+      },
+      {
+        path: '**', 
+        component: NotFoundComponent
+      },
+    ])
   ],
-  providers: [],
+  providers: [
+    GithubFollowersService,
+    { provide: ErrorHandler, useClass: AppErrorHandler }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
